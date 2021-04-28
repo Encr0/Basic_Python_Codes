f7cae7b55818900f39aeb4f26210c4ebfe2e4fb8
Wildnest <81879938+Encr0@users.noreply.github.com>
2021-04-28 09:43:30 -0400

    Update Circle_pattern.py

--- python projects/Circle_pattern.py
+++ python projects/Circle_pattern.py
@@ -4,7 +4,7 @@ turtle.bgcolor("black")
 turtle.pensize(3)
 turtle.speed(0)
 
-b=["white","red","cyan","magenta"]
+b=["white","red","cyan","magenta"] #u can change colors btw / puedes cambiar los colores si quieres (nombres de colores en ingles)
 for i in range(15):
     for a in b:
         turtle.color(a)
