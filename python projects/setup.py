2f93a79e533d07fb232b771ed653738329d73e45
Pablo <81879938+Encr0@users.noreply.github.com>
2021-04-28 09:52:53 -0400

    Update setup.py

--- python projects/setup.py
+++ python projects/setup.py
@@ -1,7 +1,12 @@
+#transforma archivos python a exe 
+#instalar cx_freeze y idna en cmd 
+#pip intall cx_freeze 
+#pip install idna 
+
 from cx_Freeze import setup, Executable
 
 base = None
-executables = [Executable("love_calc.py", base=base)]
+executables = [Executable("love_calc.py", base=base)] # donde dice love_calc.py reemplazar con el nombre de tu archivo .py 
 packages = ["idna"]
 options = {
     'build_exe': {
